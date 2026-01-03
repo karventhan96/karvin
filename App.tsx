4dac520969a058ec35098c61b7320e6f54ae822a
karventhan96 <karventhan.kar@gmail.com>
2026-01-03 23:42:02 +0530

    [Fix]: update main file

--- App.tsx
+++ App.tsx
@@ -4,17 +4,17 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>My first app</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+                                         container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
+              alignItems: "center",
     justifyContent: "center",
   },
 });
